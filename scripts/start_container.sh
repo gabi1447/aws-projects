e6bcc8eda337b73c5f60567b13bdc2315ac33e0e
Gabriel <90766958+gabi1447@users.noreply.github.com>
2024-05-29 09:04:16 +0100

    adding files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pull the Docker image from docker hub 
+
+docker pull gabi1447/aws-code-build-python-flask:latest
+
+# Run the Docker image as a container
+
+docker run -d -p 5000:5000 gabi1447/aws-code-build-python-flask:latest
