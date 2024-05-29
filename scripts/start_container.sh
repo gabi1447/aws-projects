8fa2fbb632e6ee5f4b216b034d533f7497bc5cfc
Gabriel <90766958+gabi1447@users.noreply.github.com>
2024-05-29 10:09:07 +0100

    modifying scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull gabi1447/aws-code-build-python-flask:latest
 
 # Run the Docker image as a container
 
-docker run -d -p 5000:5000 gabi1447/aws-code-build-python-flask:latest
+docker run -d --name python-flask -p 5000:5000 gabi1447/aws-code-build-python-flask:latest 
---
+++ scripts/stop_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# stop and delete docker container if any
+
+docker stop python-flask && docker rm python-flask
