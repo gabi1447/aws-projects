681a7a5a1eee02d39dcf8483fe4e41004451d121
Gabriel Pacheco <gabrielpachecotec@gmail.com>
2024-05-30 12:52:54 +0100

    moving files to another repo

--- scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pull the Docker image from docker hub 
-
-docker pull gabi1447/aws-code-build-python-flask:latest
-
-# Run the Docker image as a container
-
-docker run -d --name python-flask -p 5000:5000 gabi1447/aws-code-build-python-flask:latest 
--- scripts/stop_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# stop and delete docker container if any
-
-CONTAINER_ID=`docker ps | awk -f " " '{print $1}'`
-docker rm -f $CONTAINER_ID
